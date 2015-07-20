273786a0e830bd582294419c5a93211552e692ba
Ian McFarlane <ianm@cybernetics.com>
2015-07-20 11:30:13 -0400

    Add int detection to interpreting results

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='pash',
-    version='1.1.1',
+    version='1.1.2',
     description="Module for interacting with os.subprocess easily.",
     long_description=readme + '\n\n' + history,
     author="Ian McFarlane",
